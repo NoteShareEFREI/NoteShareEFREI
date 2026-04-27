6eded7ded33b9027e0a475a6512079e34bd213e4
Maitret17 <tombieleg@gmail.com>
2026-04-27 15:14:57 +0200

    feat: add css + css handler

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("files"))))
     http.HandleFunc("/", routers.Handler)
 	http.HandleFunc("/home", routers.HomeHandler)
     log.Fatal(http.ListenAndServe(":8080", nil))
