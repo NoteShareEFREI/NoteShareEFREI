f77ced9b3c027150f2b2ea8addec29bdf9e4b907
AlexisLaf <150516817+AlexisLaf@users.noreply.github.com>
2026-05-01 23:35:02 +0200

    Adding dependencies
    Added the dependencies to be used and the shell of a function that should be used to connect to the database.
    Look at the documentation to continue.

---
+++ database/connection.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"database/sql"
+	"github.com/go-sql-driver/mysql"
+)
+
+func Connection() {
+	/*user := "1"
+	pass := "2"
+	dbname := "3"
+
+	//Create a connection
+	//db := sql.Open("mysql","tcp", "", "127.0.0.1:3306", user, pass, dbname)
+
+	err := db.Connect()
+	if err != nil {
+		panic(err)
+	}
+	//TO BE IMPLEMENTED
+	*/
+}
+
+func Doquery(string) bool {
+	panic("to remove errors")
+}
