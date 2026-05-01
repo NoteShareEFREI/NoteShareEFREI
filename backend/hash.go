2cb39d7123eac215afd82c9aab68410fd54ecf43
AlexisLaf <150516817+AlexisLaf@users.noreply.github.com>
2026-05-01 23:26:02 +0200

    Create an account
    The Account handler is 60% finished.
    What is needed now is to add the database and then add the requests to finish it.

--- backend/hash.go
+++ backend/hash.go
@@ -1,13 +1,22 @@
 package backend
 
 import (
+	"math/rand"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
 // Called when a new user account is created.
-func NewHash(password string, name string) (string, error) {
-	//TO BE IMPLEMENTED
-	panic("Not yet Implemented")
+func NewHash(password string, name string) (string, int, error) {
+	salt_rounds := rand.Intn(15) + 10 //Random number between 11 and 25
+	//The salt rounds represents the number of time the string will be hashed.
+	//It being random makes it harder to calculate the original password but we need to be carefull for it to not to be to high as this is ressource intensive.
+	hash, err := HashPassword(password, salt_rounds)
+	if err != nil {
+		return "", 0, err
+	}
+
+	return hash, salt_rounds, nil
 }
 
 // Called when the user logs in
--- main.go
+++ main.go
@@ -20,10 +20,13 @@ func main() {
 	http.HandleFunc("/", routers.Handler)
 	http.HandleFunc("/home", routers.HomeHandler)
 	http.HandleFunc("/login", routers.LoginHandler)
+	http.HandleFunc("/signup", routers.CreateHandler)
 	http.Handle("/account", backend.Accountmiddleware(http.HandlerFunc(routers.AccountHandler)))
 
 	//API end points (http responses with no html)
 	http.HandleFunc("/api/login", api.LoginHandler)
+	http.HandleFunc("/api/signup", api.CreateHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	//http.ListenAndServeTLS() //Serve over https (need to create certificate and key with openssl)(Requires 'personal' information and the domain name on which the website is hsoted (FQDN))
 }
